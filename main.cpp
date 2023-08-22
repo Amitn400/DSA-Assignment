947edf404427a85382bfd052ad37c627657afb88
Amitn400 <141478429+Amitn400@users.noreply.github.com>
2023-08-22 17:04:06 +0000

    Main Cpp initial push

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout<<"Enter first number : " << endl;
+    cin >> a;
+    cout<<"Enter second number : " << endl;
+    cin >> b;
+    cout<<"Enter third number : " << endl;
+    cin >> c;
+    int addition = a+b+c;
+    cout << "The answer is " << addition << endl;
+    
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+rst number : " << endl;
+    cin >> a;
+    cout<<"Enter second number : " << endl;
+    cin >> b;
+    cout<<"Enter third number : " << endl;
+    cin >> c;
+    int addition = a+b+c;
+    cout << addition << endl;
